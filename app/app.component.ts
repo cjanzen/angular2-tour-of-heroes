ffcfa432712d989ae4ec3a9f197dbbf478eee881
cjanzen <techcommdev+GitHub@gmail.com>
2016-07-07 16:01:09 -0500

    Working on routers

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+import { HeroService } from './hero.service';
+import { HeroesComponent} from './heroes.component';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <a [routerLink]="['/heroes']">Heroes</a>
+    <my-heroes></my-heroes>
+  `,
+  directives: [HeroesComponent],
+  providers: [
+    HeroService
+  ]
+})
+export class AppComponent {
+  title = 'Tour of Heroes 2';
+}
